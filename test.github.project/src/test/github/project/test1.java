05ced8ad833e4c2836b74d3852f3e2a63896491b
Farooque Azam <Farooque Azam@DESKTOP-CN6P3FG>
2022-01-30 10:28:01 +0500

    First Commit to the Master Branch

---
+++ test.github.project/src/test/github/project/test1.java
@@ -0,0 +1,18 @@
+package test.github.project;
+
+public class test1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		helloWorld();
+	}
+
+	/**
+	 * 
+	 */
+	public static void helloWorld() {
+		System.out.print("Hello World");
+	}
+
+}
